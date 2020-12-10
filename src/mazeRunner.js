877eb12e35df0d2876d67c8b19319590bb28bbd7
Martin Rodriguez <martin.rdfg@gmail.com>
2020-12-10 01:45:11 -0300

    maze runner

--- src/mazeRunner.js
+++ src/mazeRunner.js
@@ -62,9 +62,6 @@ class MazeRunner{
         const destination = this.matrix.get([i, j]);
         const solutionValues = this.route.map((e) => { if (e.value !== this.startBlock.value) return e.value; }).join('');
         let seq = `${solutionValues}${destination}`;
-        if(seq.length < 3){
-            return true;
-        }
         if (seq.length === 4){
             this.reorderSequence(seq);
         }
